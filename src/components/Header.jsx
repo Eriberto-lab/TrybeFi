51aa89bd41cadffa13d5e18fc28b4fae983fdcf9
Eriberto Lima <eribertosflima@gmail.com>
2022-11-11 21:53:19 -0300

    requisito 04

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -23,9 +24,16 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         <div>Header</div>
-        <p data-testid="header-user-name">
+        <div>
+          <Link data-testid="link-to-search" to="/search">Pesquise</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favoritos</Link>
+          <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+
+        </div>
+
+        <span data-testid="header-user-name">
           {isLoading ? user : <Loading /> }
-        </p>
+        </span>
       </header>
 
     );
